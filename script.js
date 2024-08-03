fbd3f59a100a6d7fc02a64d460135cc2fc5f6665
G33kk33g <moloyedeadeyinka@outlook.com>
2024-08-03 14:37:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    if (email === '' || password === '') {
+        alert('Please enter both email and password.');
+        event.preventDefault(); // Prevent the form from submitting
+    }
+});
